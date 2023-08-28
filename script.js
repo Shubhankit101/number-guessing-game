372f720485658551a82c0208992e126eea6243fd
Shubhankit101 <45731902+Shubhankit101@users.noreply.github.com>
2023-08-29 02:20:14 +0530

    code changes

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
 'use strict';
-//Selecting an Element
-console.log(document.querySelector('.message').textContent);
 
-//DOM
+document.querySelector('.check').addEventListener('click', function() {
+const guess = Number(document.querySelector('.guess').value);    
+//console.log(document.querySelector('.guess').value);   
+console.log(guess, typeof guess);
 
-/*
-Document Object Model: Structured representation of HTML Document.
-Allows JS to access HTML element and styles to Manipulte them.
-
-DOM is automaticallly created by the browser as soon as 
-the html page loads and its stored in a tree structure like this one.
-
-*/
-
-document.querySelector('.message').textContent = '😀 Correct Number!';
-
-document.querySelector('.number').textContent = 21;
-document.querySelector('.score').textContent = 69;
-
-document.querySelector('.guess').value = 67;
-console.log(document.querySelector('.guess').value);
+if(!guess) {
+    document.querySelector('.message').textContent = 'Number Not Present!';
+}
+});
