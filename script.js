d726c0b4a535c925d07be9fabdd1cb391311b813
Shubhankit101 <45731902+Shubhankit101@users.noreply.github.com>
2023-09-12 12:07:24 +0530

    Adding Again button functionality

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 //number will remain between 1 and 20. As well decimal values will be removed
-document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.number').textContent = '?';
 //Creating a score variable 
 let score = 20;
-
+let highScore = 0;
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     //console.log(document.querySelector('.guess').value);   
@@ -17,6 +17,7 @@ document.querySelector('.check').addEventListener('click', function() {
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = 'Congratulations you Guessed Correctly';
         document.querySelector('body').style.backgroundColor = '#60b347'
+        document.querySelector('.number').textContent = secretNumber;
 
         document.querySelector('.number'). style.width = '30rem';
     }
@@ -42,4 +43,17 @@ document.querySelector('.check').addEventListener('click', function() {
             document.querySelector('.score').textContent = 0;
         }
     }
+});
+
+document.querySelector('.again').addEventListener('click', function() {
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent = 20;
+    document.querySelector('body').style.backgroundColor = '#404079';
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('.number'). style.width = '15rem';
+
+    // document.querySelector('.highscore').textContent = highScore;
 });
