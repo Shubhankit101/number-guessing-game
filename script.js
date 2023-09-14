c5fafd8ef5e61442af8f49be8df3db1b78ec42fa
Shubhankit101 <45731902+Shubhankit101@users.noreply.github.com>
2023-09-14 14:20:04 +0530

    Code fix

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+'use script'
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 //number will remain between 1 and 20. As well decimal values will be removed
@@ -7,10 +8,6 @@ let score = 20;
 let highScore = 0;
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
-    //console.log(document.querySelector('.guess').value);   
-    console.log(guess, typeof guess);
-
-
     if (!guess) {
         document.querySelector('.message').textContent = 'Number Not Present!';
     } else if (guess === secretNumber) {
